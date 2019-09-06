41bc2bd7f90bc44bdfa8af2595071df24165f228
iqbal <emixbal@gmail.com>
2019-09-06 10:10:27 +0700

    add bodyParser

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+const bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3030);
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// parse application/json
+app.use(bodyParser.json())
 
 
 app.get('/', function (req, res) {
     return res.send('root endpoint');
 });
 
-app.post('send_message', function(req, res){
+app.post('/send_message', function(req, res){
     const {pesan, no_hp} = req.body
     const data = {
         "pesan":pesan,
         "no_hp":no_hp
-    }
+    }   
     io.emit(`chatpesan:${pesan.to}`, pesan);
 })
 
