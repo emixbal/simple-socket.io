d807840f25d567d47ed95435e5cebcdff9610eef
iqbal <emixbal@gmail.com>
2019-09-16 15:48:31 +0700

    add JWT in socket

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 app.set('port', process.env.PORT || 3030);
 
@@ -12,20 +14,30 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 
 
+
 app.get('/', function (req, res) {
     return res.send('root endpoint');
 });
 
-app.post('/send_message', function(req, res){
-    const {pesan, no_hp} = req.body
+app.post('/send_message', function(req, res, next){
+    if (!req.headers.authorization) {
+        return res.status(403).json({ error: 'Aauthorization token is not provied!' });
+    }
+    const token = req.headers.authorization
+    var user = jwt.verify(token, process.env.SECRET_KEY);
+
+    console.log(user);
+
+    const { pesan, no_hp } = req.body
     const data = {
         "pesan":pesan,
         "no_hp":no_hp
     }
     io.on('connection', function (socket) {
-        io.emit(`chatpesan:2`, data);
+        io.emit(`chatpesan:${user.id}`, data);
     });
     res.send({"hallo":"hallo"});
+    next()
 })
 
 app.post("/foo", function(req, res, next) {
