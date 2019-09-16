abde9093fa9bb2e32b1d6cafaf3eaa65ebff039b
iqbal <emixbal@gmail.com>
2019-09-16 21:58:59 +0700

    making middleware ensure token

--- index.js
+++ index.js
@@ -13,30 +13,58 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json())
 
+function ensureToken(req, res, next) {
+  var bearerHeader = req.headers['authorization']
+  if (typeof bearerHeader=='undefined'){
+    console.log('undefined, gak ada token');
+    return res.status(403).send({
+      'message':'token not provided'
+    })
+  } else {
+    var bearerToken = bearerHeader.split(' ')
+    var tokenBracket = bearerToken[0]
+    var tokenProvided = bearerToken[1]
+
+    if(tokenBracket !== 'bearer'){
+      return res.status(403).send({
+        'message':'worng format header Authorization'
+      })
+    }
+
+    jwt.verify(tokenProvided, process.env.SECRET_KEY, function(err, decoded) {
+      if(err){
+        console.log('token is not valid');
+        return res.status(403).send({
+          'message':'token is not valid'
+        })
+        next()
+      }
+      return req.body.user = decoded
+      next()
+    });
+  }
+  next()
+}
+
 app.get('/', function (req, res) {
     return res.send('root endpoint');
 });
 
-app.post('/send_message', function(req, res, next){
-    if (!req.headers.authorization) {
-        return res.status(403).json({ error: 'Aauthorization token is not provied!' });
-    }
+app.post('/send_message', ensureToken, function(req, res, next){
     if(typeof(req.body.message)=='undefined' || typeof(req.body.phone_number)=='undefined')
         return res.status(422).send({"message":"message or phone number can't be null!"})
-    
-        const token = req.headers.authorization
-    var user = jwt.verify(token, process.env.SECRET_KEY);
 
+    const userData = req.body.user //get from middleware
     const data = {
         "message":req.body.message,
         "phone_number":req.body.phone_number
     }
-    
+
     // io.on('connection', function (socket) {
     //     io.emit(`sendsms:${user.id}`, data);
     // });
-    io.emit(`sendsms:${user.id}`, data);
-    
+    io.emit(`sendsms:${userData.id}`, data);
+
     return res.send({"message":"ok"});
     next()
 })
